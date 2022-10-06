210afea67938ac526b5f091ee9b148245b726ad0
emmarivera <emrp077@gmail.com>
2022-10-06 02:16:15 -0500

    version3

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './styles/locationInfo.css'
 
 const LocationInfo = ({location}) => {
 
   return (
-    <article>
+    <article className='card__location-info'>
         <h2>{location?.name}</h2>
-        <ul>
+        <ul className='ul__location-info'>
             <li><span>Type: </span>{location?.type}</li>
             <li><span>Dimension: </span>{location?.dimension}</li>
             <li><span>Population: </span>{location?.residents.length}</li>
